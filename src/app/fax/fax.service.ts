73da0ee351ab1f77735fad5ad093ab2b288f2769
Stefan Niedermann <info@niedermann.it>
2021-09-06 11:42:00 +0200

    Add support for multiple pages

--- src/app/fax/fax.service.ts
+++ src/app/fax/fax.service.ts
@@ -9,7 +9,12 @@ export class FaxService {
   private _y = 25;
 
   private readonly x = 20;
-  private get y() {
+
+  private getY(doc: jsPDF): number {
+    if(this._y > doc.internal.pageSize.height - 30) {
+      doc.addPage()
+      this._y = 25;
+    }
     return this._y += 4.5;
   }
 
@@ -26,53 +31,53 @@ export class FaxService {
 
     doc.setTextColor('#000000');
     doc.setFontSize(11)
-    doc.text("------ FAX ------ FAX ------ FAX ------ FAX ------ FAX ------ FAX -------", this.x, this.y);
-    doc.text("Absender   : ILS MITTELFRANKEN SÜD", this.x, this.y);
-    doc.text(`Fax        : +49  (${this.random(4)}) / ${this.random(7)}`, this.x, this.y);
-    doc.text("Termin     :", this.x, this.y);
-    doc.text(`Einsatznummer: T ${this.random()}.${this.random()} ${this.random(6)} ${this.random(2)}`, this.x, this.y);
-    doc.text('------------------------------  MITTEILER  ------------------------------', this.x, this.y);
+    doc.text("------ FAX ------ FAX ------ FAX ------ FAX ------ FAX ------ FAX -------", this.x, this.getY(doc));
+    doc.text("Absender   : ILS MITTELFRANKEN SÜD", this.x, this.getY(doc));
+    doc.text(`Fax        : +49  (${this.random(4)}) / ${this.random(7)}`, this.x, this.getY(doc));
+    doc.text("Termin     :", this.x, this.getY(doc));
+    doc.text(`Einsatznummer: T ${this.random()}.${this.random()} ${this.random(6)} ${this.random(2)}`, this.x, this.getY(doc));
+    doc.text('------------------------------  MITTEILER  ------------------------------', this.x, this.getY(doc));
     this.insertParagraph(doc, value.mitteiler);
-    doc.text('------------------------------  EINSATZORT  -----------------------------', this.x, this.y);
-    doc.text(`Straße     : ${value.einsatzort.strasse}`, this.x, this.y);
-    doc.text(`Haus-Nr.   : ${value.einsatzort.hnr}`, this.x, this.y);
-    doc.text(`Ort        : ${value.einsatzort.ort}`, this.x, this.y);
-    doc.text(`Objekt     : ${value.einsatzort.objekt}`, this.x, this.y);
-    doc.text('Station    :', this.x, this.y);
-    doc.text('', this.x, this.y);
-    doc.text('X=', this.x, this.y);
-    doc.text('Y=', this.x, this.y);
-    doc.text('', this.x, this.y);
-    doc.text('------------------------------  ZIELORT  --------------------------------', this.x, this.y);
-    doc.text(`Straße     : ${value.zielort.strasse}	Haus-Nr.: ${value.zielort.hnr}`, this.x, this.y);
-    doc.text(`Ort        : ${value.zielort.ort}`, this.x, this.y);
-    doc.text(`Objekt     : ${value.zielort.objekt}`, this.x, this.y);
-    doc.text(`Station    :`, this.x, this.y);
-    doc.text('', this.x, this.y);
-    doc.text('------------------------------  PATIENT  --------------------------------', this.x, this.y);
+    doc.text('------------------------------  EINSATZORT  -----------------------------', this.x, this.getY(doc));
+    doc.text(`Straße     : ${value.einsatzort.strasse}`, this.x, this.getY(doc));
+    doc.text(`Haus-Nr.   : ${value.einsatzort.hnr}`, this.x, this.getY(doc));
+    doc.text(`Ort        : ${value.einsatzort.ort}`, this.x, this.getY(doc));
+    doc.text(`Objekt     : ${value.einsatzort.objekt}`, this.x, this.getY(doc));
+    doc.text('Station    :', this.x, this.getY(doc));
+    doc.text('', this.x, this.getY(doc));
+    doc.text('X=', this.x, this.getY(doc));
+    doc.text('Y=', this.x, this.getY(doc));
+    doc.text('', this.x, this.getY(doc));
+    doc.text('------------------------------  ZIELORT  --------------------------------', this.x, this.getY(doc));
+    doc.text(`Straße     : ${value.zielort.strasse}	Haus-Nr.: ${value.zielort.hnr}`, this.x, this.getY(doc));
+    doc.text(`Ort        : ${value.zielort.ort}`, this.x, this.getY(doc));
+    doc.text(`Objekt     : ${value.zielort.objekt}`, this.x, this.getY(doc));
+    doc.text(`Station    :`, this.x, this.getY(doc));
+    doc.text('', this.x, this.getY(doc));
+    doc.text('------------------------------  PATIENT  --------------------------------', this.x, this.getY(doc));
     this.insertParagraph(doc, value.patient);
-    doc.text('------------------------------  EINSATZGRUND  ---------------------------', this.x, this.y);
-    doc.text(`Schlagw.   : ${value.einsatzgrund.schlagwort}`, this.x, this.y);
-    doc.text(`Stichwort  : ${value.einsatzgrund.stichwort}`, this.x, this.y);
-    doc.text('', this.x, this.y);
+    doc.text('------------------------------  EINSATZGRUND  ---------------------------', this.x, this.getY(doc));
+    doc.text(`Schlagw.   : ${value.einsatzgrund.schlagwort}`, this.x, this.getY(doc));
+    doc.text(`Stichwort  : ${value.einsatzgrund.stichwort}`, this.x, this.getY(doc));
+    doc.text('', this.x, this.getY(doc));
     if (value.einsatzmittel.length > 0) {
-      doc.text('------------------------------  EINSATZMITTEL  --------------------------', this.x, this.y);
+      doc.text('------------------------------  EINSATZMITTEL  --------------------------', this.x, this.getY(doc));
       for (let einsatzmittel of value.einsatzmittel) {
-        doc.text(`Name       : ${einsatzmittel.name}`, this.x, this.y);
-        doc.text(`Alarmiert  : ${einsatzmittel.alarmiert}`, this.x, this.y);
-        doc.text(`Aus        : ${einsatzmittel.aus}`, this.x, this.y);
+        doc.text(`Name       : ${einsatzmittel.name}`, this.x, this.getY(doc));
+        doc.text(`Alarmiert  : ${einsatzmittel.alarmiert}`, this.x, this.getY(doc));
+        doc.text(`Aus        : ${einsatzmittel.aus}`, this.x, this.getY(doc));
       }
     }
-    doc.text('', this.x, this.y);
-    doc.text('------------------------------  BEMERKUNG  ------------------------------  ', this.x, this.y);
+    doc.text('', this.x, this.getY(doc));
+    doc.text('------------------------------  BEMERKUNG  ------------------------------  ', this.x, this.getY(doc));
     this.insertParagraph(doc, value.bemerkung);
-    doc.text('--------------------------  ALARMFAX ENDE  ------------------------------', this.x, this.y);
-    doc.text('', this.x, this.y);
-    doc.text('Rechtlicher Hinweis:', this.x, this.y);
-    doc.text('Die Inhalte dieses Faxes, insbesondere personenbezogene Daten, dürfen', this.x, this.y);
-    doc.text('ausschließlich für einsatzbezogene Zwecke verwendet werden. Der Empfänger', this.x, this.y);
-    doc.text('hat sicherzustellen, dass unbefugte Dritte keinen Zugang zu diesen', this.x, this.y);
-    doc.text('Informationen erhalten.', this.x, this.y);
+    doc.text('--------------------------  ALARMFAX ENDE  ------------------------------', this.x, this.getY(doc));
+    doc.text('', this.x, this.getY(doc));
+    doc.text('Rechtlicher Hinweis:', this.x, this.getY(doc));
+    doc.text('Die Inhalte dieses Faxes, insbesondere personenbezogene Daten, dürfen', this.x, this.getY(doc));
+    doc.text('ausschließlich für einsatzbezogene Zwecke verwendet werden. Der Empfänger', this.x, this.getY(doc));
+    doc.text('hat sicherzustellen, dass unbefugte Dritte keinen Zugang zu diesen', this.x, this.getY(doc));
+    doc.text('Informationen erhalten.', this.x, this.getY(doc));
 
     return doc.output('blob');
   }
@@ -87,9 +92,9 @@ export class FaxService {
     while (filledValue.split('\n').length < 4) {
       filledValue += '\n';
     }
-    doc.text(filledValue, this.x, this.y);
+    doc.text(filledValue, this.x, this.getY(doc));
     for (let i = 1; i < (filledValue || '').split('\n').length; i++) {
-      this.y;
+      this.getY(doc);
     }
   }
 
