5fa2f94d34407fe60f768ec65ed253c268b3e93b
Stefan Niedermann <info@niedermann.it>
2021-11-19 14:45:27 +0100

    Extend e2e tests to verify the document being rendered

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,34 @@
 describe('Fax-Generator main screen', () => {
 
-  it('Visits the initial project page', () => {
+  const sampleText = "Lorem Ipsum dolor sit amet.";
+
+  beforeEach(() => {
     cy.visit('/');
-    cy.contains("Fax-Generator")
-  })
-})
+  });
+
+  it('should display the headline', () => {
+    cy.contains("Fax-Generator");
+  });
+
+  it('should toggle the sections when clicking on them', () => {
+    cy.get('[data-test="mitteiler-input"]').should('not.be.visible');
+    cy.get('[data-test="mitteiler-panel"]').click();
+    cy.get('[data-test="mitteiler-input"]').should('be.visible');
+    cy.get('[data-test="mitteiler-panel"]').click();
+    cy.get('[data-test="mitteiler-input"]').should('not.be.visible');
+  });
+
+  it('should display entered information on the page', () => {
+    cy.contains(sampleText).should('not.exist');
+    cy.get('[data-test="mitteiler-panel"]').click();
+    cy.get('[data-test="mitteiler-input"]').type(sampleText);
+    cy.contains(sampleText).should('exist');
+  });
+
+  it('should update the URL after changing the input data', () => {
+    cy.url().should('not.include', sampleText);
+    cy.get('[data-test="mitteiler-panel"]').click();
+    cy.get('[data-test="mitteiler-input"]').type(sampleText);
+    cy.url().should('include', encodeURIComponent(sampleText));
+  });
+});
