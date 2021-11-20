ec85e79de04bceb6840f15fca2b5adec047b9e39
Stefan Niedermann <info@niedermann.it>
2021-11-20 12:40:56 +0100

    Extend e2e tests

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,6 +1,6 @@
-describe('Fax-Generator main screen', () => {
+describe('the exercise operation fax generator', () => {
 
-  const sampleText = "Lorem Ipsum dolor sit amet.";
+  const sampleText = "Lorem Ipsum.";
 
   beforeEach(() => {
     cy.visit('/');
@@ -10,25 +10,55 @@ describe('Fax-Generator main screen', () => {
     cy.contains("Fax-Generator");
   });
 
-  it('should toggle the sections when clicking on them', () => {
-    cy.get('[data-test="mitteiler-input"]').should('not.be.visible');
-    cy.get('[data-test="mitteiler-panel"]').click();
-    cy.get('[data-test="mitteiler-input"]').should('be.visible');
-    cy.get('[data-test="mitteiler-panel"]').click();
-    cy.get('[data-test="mitteiler-input"]').should('not.be.visible');
+  describe('the form', () => {
+    it('should toggle the sections when clicking on them', () => {
+      cy.get('[data-test="mitteiler-input"]').should('not.be.visible');
+      cy.get('[data-test="mitteiler-panel"]').click();
+      cy.get('[data-test="mitteiler-input"]').should('be.visible');
+      cy.get('[data-test="mitteiler-panel"]').click();
+      cy.get('[data-test="mitteiler-input"]').should('not.be.visible');
+    });
+
+    it('should update the URL after changing the input data', () => {
+      cy.url().should('not.include', sampleText);
+      cy.get('[data-test="mitteiler-panel"]').click();
+      cy.get('[data-test="mitteiler-input"]').type(sampleText);
+      cy.url().should('include', encodeURIComponent(sampleText));
+    });
   });
 
-  it('should display entered information on the page', () => {
-    cy.contains(sampleText).should('not.exist');
-    cy.get('[data-test="mitteiler-panel"]').click();
-    cy.get('[data-test="mitteiler-input"]').type(sampleText);
-    cy.contains(sampleText).should('exist');
+  describe('the live preview', () => {
+    it('should always contain a hint that this is an exercise fax', () => {
+      cy.contains('ÜBUNGS-FAX');
+    });
+
+    it('should display entered information', () => {
+      cy.contains(sampleText).should('not.exist');
+      cy.get('[data-test="mitteiler-panel"]').click();
+      cy.get('[data-test="mitteiler-input"]').type(sampleText);
+      cy.contains(sampleText).should('exist');
+    });
   });
 
-  it('should update the URL after changing the input data', () => {
-    cy.url().should('not.include', sampleText);
-    cy.get('[data-test="mitteiler-panel"]').click();
-    cy.get('[data-test="mitteiler-input"]').type(sampleText);
-    cy.url().should('include', encodeURIComponent(sampleText));
+  describe('the downloadable PDF version', () => {
+    it('should always contain a hint that this is an exercise fax', () => {
+      cy.wait(1_000);
+      cy.get('[data-test="download-button"]').click();
+      const date = new Date();
+      cy.task('getPdfContent', `Übungs-Fax ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.pdf`).then(content => {
+        expect(content.text).to.contain('ÜBUNGS-FAX');
+      });
+    });
+
+    it('should pass the entered text to the downloadable PDF version', () => {
+      cy.get('[data-test="mitteiler-panel"]').click();
+      cy.get('[data-test="mitteiler-input"]').type(sampleText);
+      cy.wait(1_000);
+      cy.get('[data-test="download-button"]').click();
+      const date = new Date();
+      cy.task('getPdfContent', `Übungs-Fax ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.pdf`).then(content => {
+        expect(content.text).to.contain(sampleText);
+      });
+    });
   });
 });
--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,3 +1,14 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
-module.exports = (on, config) => {}
+
+const fs = require('fs');
+const path = require('path');
+const pdf = require('pdf-parse');
+
+const parsePdf = 
+
+module.exports = (on, config) => {
+  on('task', {
+    getPdfContent: (pdfName) => pdf(fs.readFileSync(path.join('cypress/downloads/', pdfName)))
+  })
+};
